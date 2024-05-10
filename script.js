1d9c7211dc082bac801b2e5761ad8e1ddab454bc
Neeraj Chaudhary <36655557+naspnrd@users.noreply.github.com>
2024-05-10 19:40:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 function firstWord(str) {
- // Trim the string to remove leading and trailing whitespace
-  const trimmedStr = str.trim(); 
-  
-  // Split the trimmed string by space
-  const words = trimmedStr.split(' ');
-  
-  // Return the first word or the entire string if it's empty or has no space
-  return words.length > 0 ? words[0] : trimmedStr;
+ 
 }
 
 // Do not change the code below
