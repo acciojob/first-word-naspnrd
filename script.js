88ebca79bf872228fec0a64c1925ee214ff9410d
Neeraj Chaudhary <36655557+naspnrd@users.noreply.github.com>
2024-05-10 22:56:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function firstWord(str) {
- 
+	const trimmedStr = str.trim();
+	const words = trimmedStr.split(" ");
+
+	return words.length > 0 ? words[0] : trimmedStr;
 }
 
 // Do not change the code below
