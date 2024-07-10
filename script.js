2946a63f652812a0e26ee24b9966b749a66cd9dd
Neeraj Chaudhary <36655557+naspnrd@users.noreply.github.com>
2024-07-10 23:07:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function firstWord(str) {
 	const trimmedStr = str.trim();
-	const words = trimmedStr.split(" ")
-	
+	const words = trimmedStr.split(" ");
+	console.log(words);
 	return words[0];
 }
 
+console.log(firstWord('see and stop'))
+console.log(firstWord(' Hello World!'))
+console.log(firstWord('12345'));
+console.log(firstWord(''));
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
